22e582e9f34067e37c5fe9362b1c754f146dfe39
Humayun Jawad <humayunspam143@gmail.com>
2024-03-24 15:22:29 +0500

    .htaccess file

--- assets/js/main.js
+++ assets/js/main.js
@@ -679,49 +679,49 @@
   /*--------------------------------------------------------------
     21. Dynamic contact form
   --------------------------------------------------------------*/
-  if ($.exists('#cs-form')) {
-    const form = document.getElementById('cs-form');
-    const result = document.getElementById('cs-result');
+  // if ($.exists('#cs-form')) {
+  //   const form = document.getElementById('cs-form');
+  //   const result = document.getElementById('cs-result');
 
-    form.addEventListener('submit', function (e) {
-      const formData = new FormData(form);
-      e.preventDefault();
-      var object = {};
-      formData.forEach((value, key) => {
-        object[key] = value;
-      });
-      var json = JSON.stringify(object);
-      result.innerHTML = 'Please wait...';
+  //   form.addEventListener('submit', function (e) {
+  //     const formData = new FormData(form);
+  //     e.preventDefault();
+  //     var object = {};
+  //     formData.forEach((value, key) => {
+  //       object[key] = value;
+  //     });
+  //     var json = JSON.stringify(object);
+  //     result.innerHTML = 'Please wait...';
 
-      fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        body: json,
-      })
-        .then(async response => {
-          let json = await response.json();
-          if (response.status == 200) {
-            result.innerHTML = json.message;
-          } else {
-            console.log(response);
-            result.innerHTML = json.message;
-          }
-        })
-        .catch(error => {
-          console.log(error);
-          result.innerHTML = 'Something went wrong!';
-        })
-        .then(function () {
-          form.reset();
-          setTimeout(() => {
-            result.style.display = 'none';
-          }, 5000);
-        });
-    });
-  }
+  //     fetch('https://api.web3forms.com/submit', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Accept: 'application/json',
+  //       },
+  //       body: json,
+  //     })
+  //       .then(async response => {
+  //         let json = await response.json();
+  //         if (response.status == 200) {
+  //           result.innerHTML = json.message;
+  //         } else {
+  //           console.log(response);
+  //           result.innerHTML = json.message;
+  //         }
+  //       })
+  //       .catch(error => {
+  //         console.log(error);
+  //         result.innerHTML = 'Something went wrong!';
+  //       })
+  //       .then(function () {
+  //         form.reset();
+  //         setTimeout(() => {
+  //           result.style.display = 'none';
+  //         }, 5000);
+  //       });
+  //   });
+  // }
   /*--------------------------------------------------------------
     22. Cursor Animation
   --------------------------------------------------------------*/
